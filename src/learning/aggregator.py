eeb8fecec1e1dbfcad4a874e26741f8c631a572d
Kundjanasith Thonglek <thonglek.kundjanasith.ti7@is.naist.jp>
2023-02-26 05:02:59 +0900

    pickle

--- src/learning/aggregator.py
+++ src/learning/aggregator.py
@@ -1,6 +1,7 @@
 import utils, glob
 from tensorflow.keras.backend import clear_session
 import h5py
+import pickle
 
 def getLayerIndexByName(model, layername):
     for idx, layer in enumerate(model.layers):
@@ -54,10 +55,11 @@ class Aggregator():
         model = utils.model_init()
         if global_epoch == 0:
             print('Initial model . . .')
-            model.save_weights('aggregator_storage/aggregator_models/model_ep0.h5')
-            model.load_weights('aggregator_storage/aggregator_models/model_ep0.h5')
-            res = h5py.File('aggregator_storage/aggregator_models/model_ep0.h5', 'r')
-            return res
+            # model.save_weights('aggregator_storage/aggregator_models/model_ep0.h5')
+            # model.load_weights('aggregator_storage/aggregator_models/model_ep0.h5')
+            pickle.dump(model, open('aggregator_storage/aggregator_models/model_ep0.h5','wb'))
+            model = pickle.load(open('aggregator_storage/aggregator_models/model_ep0.h5','rb'))
+            return model
         else:
             print('Load global model %d'%(global_epoch))
             model_paths = ['aggregator_storage/aggregator_models/model_ep%d.h5'%(self.global_epoch-1)]
--- src/learning/trainer.py
+++ src/learning/trainer.py
@@ -39,13 +39,13 @@ class Trainer():
         self.local_epochs = int(config['TRAINING']['LOCAL_EPOCHS'])
 
     def training(self, model):
-        print(model)
+        # print(model)
 
-        # model = utils.model_init()
-        # model.load_weights('trainer_storage/aggregator_models/model_ep%d.h5'%(global_epoch))
-        # x_train, y_train = sampling_data(self.num_samples)
-        # model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
-        # model.fit(x_train, y_train,epochs=self.local_epochs,batch_size=self.local_batch_size,verbose=1,validation_split=0.2)
-        # model.save_weights('trainer_storage/trainer_models/%s_ep%d.h5'%(hostname,global_epoch+1))
-        # return model 
+        model = utils.model_init()
+        model.load_weights('trainer_storage/aggregator_models/model_ep%d.h5'%(global_epoch))
+        x_train, y_train = sampling_data(self.num_samples)
+        model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
+        model.fit(x_train, y_train,epochs=self.local_epochs,batch_size=self.local_batch_size,verbose=1,validation_split=0.2)
+        model.save_weights('trainer_storage/trainer_models/%s_ep%d.h5'%(hostname,global_epoch+1))
+        return model 
         
