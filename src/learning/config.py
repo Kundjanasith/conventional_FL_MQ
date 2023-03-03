1553624bb34adb2f4ee8bebd5b445449945b16b3
Kundjanasith Thonglek <thonglek.kundjanasith.ti7@is.naist.jp>
2023-03-03 13:35:52 +0900

    A1(1) T5(12-16)

--- src/learning/config.py
+++ src/learning/config.py
@@ -2,13 +2,13 @@ CONFIG = {
     "training": {
         "num_communication_rounds": 1000,
         "num_samples": 100,
-        "local_batch_size": 32,
-        "local_epochs": 10,
-        "num_trainers": 10,
+        "local_batch_size": 8,
+        "local_epochs": 5,
+        "num_trainers": 5,
         "trainer_name_prefix": "trainer",
     },
     "distribution": {
-        "pyamqp_ip": "10.244.18.116",
+        "pyamqp_ip": "10.10.100.1",
         "aggregator_ip": "0.0.0.0",
         "trainer_ip": ["0.0.0.0", "0.0.0.0"],
     },
